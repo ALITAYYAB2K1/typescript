9e95b0c7c5bb2cbaf04df4ee380a3e97a103f308
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2025-05-22 16:06:54 +0500

    Add TypeScript definitions for RGB tuples and user type

--- tuples.js
+++ tuples.js
@@ -8,3 +8,6 @@ Tuser = ["ali", 23, true];
 console.log(Tuser[0].toLowerCase()); // ali
 console.log(Tuser[1].toFixed(2)); // 23.00
 console.log(Tuser[2].valueOf()); // true
+//tuples of rgb
+var rgb = [255, 143, 242];
+var newUser = [2, "email"];
--- tuples.ts
+++ tuples.ts
@@ -8,4 +8,10 @@ Tuser = ["ali", 23, true];
 console.log(Tuser[0].toLowerCase()); // ali
 console.log(Tuser[1].toFixed(2)); // 23.00
 console.log(Tuser[2].valueOf()); // true
+
+//tuples of rgb
+let rgb: [number, number, number] = [255, 143, 242];
+
+type User = [number, string];
+const newUser: User = [2, "email"];
 export {};
